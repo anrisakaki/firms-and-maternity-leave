a24d74e0b8b80f32294748364adf403c3d4210ff
Anri Sakakibara <anrisakaki@gmail.com>
2023-05-04 18:13:24 +0100

    Creating a panel of firms

--- Creating panel of firms.R
+++ Creating panel of firms.R
@@ -91,3 +91,32 @@ for(i in id){
 
 # getting namsxkd for 2011 dataset using 2012 dataset 
 
+id1112_madn <- left_join(id12, id11, by = "madn", multiple = "all") %>% 
+  mutate(correct_tinh = ifelse(tinh.x == tinh.y, 1, 0),
+         correct_huyen = ifelse(huyen.x == huyen.y, 1, 0),
+         correct_xa = ifelse(xa.x == xa.y, 1, 0),
+         correct_ma_thue = ifelse(ma_thue.x == ma_thue.y, 1, 0),
+         correct_thx = ifelse((correct_tinh + correct_huyen + correct_xa) == 3, 1, 0)) %>% 
+  filter(namsxkd != "NA")
+
+namsxkd_11 <- id1112_madn %>% select(tinh.x, huyen.x, xa.x, madn, namsxkd) %>% 
+  rename(tinh = tinh.x,
+         huyen = huyen.x,
+         xa = xa.x)
+
+dn11 <- left_join(dn11, namsxkd_11, by = c("tinh", "huyen", "xa", "madn"), multiple = "all")
+
+# Only retaining observations kept between 2008 and 2018 
+
+panel15 <- dn15 %>% select(tinh, huyen, xa, lhdn, ma_thue, ma_thue1, ma_thue2, madn)
+panel08 <- dn08 %>% select(tinh, huyen, xa, lhdn, namsxkd, ma_thue, madn)
+
+panel0815 <- merge(panel15, panel08, by = "madn") %>% 
+  mutate(correct_tinh = ifelse(tinh.x == tinh.y, 1, 0),
+         correct_huyen = ifelse(huyen.x == huyen.y, 1, 0),
+         correct_xa = ifelse(xa.x == xa.y, 1, 0),
+         correct_ma_thue = ifelse(ma_thue.x == ma_thue.y, 1, 0),
+         correct_thx = ifelse((correct_tinh + correct_huyen + correct_xa + correct_ma_thue) == 4, 1, 0))  
+
+panel0815_correct <- panel0815 %>% filter(correct_thx == 1)
+panel0815_incorrect <- panel0815 %>% filter(correct_thx == 0)
