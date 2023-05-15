0adcae61c76cfb9ca61d2e328c7a68d26080f2b9
Anri Sakakibara <anrisakaki@gmail.com>
2023-05-15 15:55:02 +0100

    TWFE

--- TWFE.R
+++ TWFE.R
@@ -2,27 +2,22 @@
 # DIFFERENCE-IN-DIFFERENCES #
 #############################
 
-dn12 <- dn12 %>% 
-  rename(ma_thue1 = ma_thue) %>% 
-  mutate(ma_thue2 = substr(ma_thue1, 10, 10),
-         ma_thue = substr(ma_thue1, 1, 9))
-
 fworkers_12 <- dn12 %>% 
-  select(tinh, huyen, xa, madn, ma_thue1, fworkers) %>% 
+  select(tinh, huyen, xa, madn, ma_thue, fworkers) %>% 
   rename(fworkers_12 = fworkers)
 
 for(i in dn0818){
   
   if(i %in% c("dn13", "dn14", "dn15")){
     
-    assign(i, left_join(get(i), fworkers_12, by = c("tinh", "huyen", "xa", "madn", "ma_thue1"), multiple = "all"))
+    assign(i, left_join(get(i), fworkers_12, by = c("tinh", "huyen", "xa", "madn", "ma_thue"), multiple = "all"))
     
   }
   
 }
 
 dn1315 <- bind_rows(dn13, dn14, dn15) %>% 
-  group_by(tinh, huyen, xa, madn, ma_thue1) %>% 
+  group_by(tinh, huyen, xa, madn, ma_thue) %>% 
   mutate(id = cur_group_id()) %>% 
   mutate(post = ifelse(year > 2013, 1, 0),
          did = post*fworkers_12)
@@ -31,10 +26,45 @@ etable(list(
   feols(fworkers ~ fworkers_12 + post + did | id,
         dn1315),
   feols(fworkers_eoy ~ fworkers_12 + post + did | id,
-        dn1215),
+        dn1315),
   feols(finformal ~ fworkers_12 + post + did | id,
         dn1315),
   feols(pretax_profit ~ fworkers_12 + post + did | id,
         dn1315)  
 ), tex = T)
 
+###############
+# EVENT STUDY #
+###############
+
+for(i in dn0818){
+  
+  if(i %in% c("dn08", "dn09", "dn10", "dn11")){
+    
+    assign(i, left_join(get(i), fworkers_12, by = c("tinh", "huyen", "xa", "madn", "ma_thue"), multiple = "all"))
+    
+  }
+  
+}
+
+dn12 <- dn12 %>% mutate(fworkers_12 = fworkers)
+
+dn0815 <- bind_rows(dn08, dn09, dn10, dn11, dn12, dn13, dn14, dn15) %>% 
+  group_by(tinh, huyen, xa, madn, ma_thue) %>% 
+  mutate(id = cur_group_id())
+
+iplot(feols(fworkers ~ i(year, fworkers_12, 2012) | id + year,
+            dn0815,
+            vcov = ~id), main = "")
+
+iplot(feols(fworkers_eoy ~ i(year, fworkers_12, 2012) | id + year,
+            dn0815,
+            vcov = ~id), main = "")
+
+iplot(feols(finformal ~ i(year, fworkers_12, 2012) | id + year,
+            dn0815,
+            vcov = ~id), main = "")
+
+iplot(feols(pretax_profit ~ i(year, fworkers_12, 2012) | id + year,
+            dn0815,
+            vcov = ~id), main = "")
