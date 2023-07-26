bf05ecbe83998ae440dabb196d9377a351406c21
Anri Sakakibara <anrisakaki@gmail.com>
2023-07-26 19:18:36 +0100

    Data cleaning

--- Data cleaning.R
+++ Data cleaning.R
@@ -8,7 +8,7 @@ dn08 <- dn08 %>%
          n_fworkers_ss = ld24,
          n_informal = ld33,
          n_finformal = ld34,         
-         wage = tn2,
+         wage = tn1,
          ss_comp = tn3,
          ss_cont = tn5,
          pretax_profit = kqkd9)
@@ -23,7 +23,7 @@ dn09 <- dn09 %>%
          n_fworkers_ss = ld24,
          n_informal = ld33,
          n_finformal = ld34,         
-         wage = tn21,
+         wage = tn11,
          ss_comp = tn31,
          ss_cont = tn91,
          pretax_profit = kqkd17)
@@ -38,7 +38,7 @@ dn10 <- dn10 %>%
          n_fworkers_ss = ld24,
          n_informal = ld33,
          n_finformal = ld34,         
-         wage = tn2,
+         wage = tn1,
          ss_comp = tn3,
          ss_cont = tn5,
          pretax_profit = kqkd17)
@@ -53,7 +53,7 @@ dn11 <- dn11 %>%
          n_fworkers_ss = ld22,
          n_informal = ld31,
          n_finformal = ld32,
-         wage = tn2,
+         wage = tn1,
          ss_comp = tn4,
          ss_cont = tn5,
          pretax_profit = kqkd20)
@@ -68,7 +68,7 @@ dn12 <- dn12 %>%
          n_fworkers_ss = ld22,
          n_informal = ld31,
          n_finformal = ld32,
-         wage = tn2,
+         wage = tn1,
          ss_comp = tn4,
          ss_cont = tn5,
          pretax_profit = kqkd22)  
@@ -83,7 +83,7 @@ dn13 <- dn13 %>%
          n_fworkers_ss = ld22,
          n_informal = ld31,
          n_finformal = ld32,
-         wage = tn2,
+         wage = tn1,
          ss_comp = tn4,
          ss_cont = tn5,
          pretax_profit = kqkd22)  
@@ -162,20 +162,40 @@ dn0818 <- c("dn08", "dn09", "dn10", "dn11", "dn12", "dn13", "dn14", "dn15", "dn1
 for(i in dn0818){
 
   assign(i, get(i) %>%
-           mutate(fworkers = n_fworkers/ n_workers,
-                  fworkers_eoy = n_fworkers_eoy/n_workers_eoy))
+           mutate(n_fworkers = ifelse(is.na(n_fworkers), 0 , n_fworkers),
+                  n_fworkers_eoy = ifelse(is.na(n_fworkers_eoy), 0 , n_fworkers_eoy),
+                  fworkers = n_fworkers/ n_workers,
+                  fworkers_eoy = n_fworkers_eoy/n_workers_eoy,
+                  fworkers = ifelse(n_workers == 0, NA, fworkers),
+                  fworkers = ifelse(n_workers > 0 & n_fworkers == 0, 0, fworkers),
+                  fworkers_eoy = ifelse(n_workers_eoy == 0, NA, fworkers_eoy),
+                  fworkers_eoy = ifelse(n_workers_eoy > 0 & n_fworkers_eoy == 0, 0, fworkers_eoy),
+                  across(vsic, as.numeric),
+                  fworkers = ifelse(fworkers > 1 | fworkers < 0, NA, fworkers),
+                  fworkers_eoy = ifelse(fworkers_eoy > 1 | fworkers_eoy < 0, NA, fworkers_eoy))
+         
+         )
 
   if(i %in% c("dn08", "dn09", "dn10", "dn11", "dn12", "dn13", "dn14", "dn15")){
     assign(i, get(i) %>%
-             mutate(finformal = n_finformal/n_informal,
-                    f_ss = n_fworkers_ss/n_workers_ss))
+             mutate(n_finformal = ifelse(is.na(n_finformal), 0, n_finformal),
+                    n_fworkers_ss = ifelse(is.na(n_fworkers_ss), 0, n_fworkers_ss),
+                    finformal = n_finformal/n_informal,
+                    f_ss = n_fworkers_ss/n_workers_ss,
+                    finformal = ifelse(n_workers == 0, NA, finformal),
+                    finformal = ifelse(n_informal > 0 & n_finformal == 0, 0, finformal),
+                    f_ss = ifelse(n_workers == 0, NA, f_ss),
+                    f_ss = ifelse(n_workers_ss > 0 & n_fworkers_ss == 0, 0, f_ss),
+                    finformal = ifelse(finformal > 1 | finformal < 0, NA, finformal),
+                    f_ss = ifelse(f_ss > 1 | f_ss < 0, NA, f_ss))
+           )
   }
-  
+
   if(i %in% c("dn08", "dn09", "dn10", "dn11")){
     assign(i, get(i) %>%
              mutate(ma_thue = paste0(ma_thue, ma_thue2, sep = "")))
-  } 
-  
+  }
+
 
   if(i %in% c("dn08", "dn09", "dn10", "dn12")){
 
@@ -203,9 +223,6 @@ for(i in dn0818){
 
   }
 
-  assign(i, get(i) %>%
-           mutate_if(is.numeric, ~ifelse(is.infinite(.), 0, .)) %>%
-           mutate(across(vsic, as.numeric)))
 
 }
 
@@ -220,3 +237,9 @@ dn15 <- dn15 %>% mutate(year = 2015)
 dn16 <- dn16 %>% mutate(year = 2016)
 dn17 <- dn17 %>% mutate(year = 2017)
 dn18 <- dn18 %>% mutate(year = 2018)
+
+dn <- bind_rows(dn08, dn09, dn10, dn11, dn12, dn13, dn14, dn15, dn16, dn17, dn18) %>% 
+  group_by(tinh, huyen, xa, ma_thue) %>% 
+  mutate(id = cur_group_id())
+
+save(dn0818, file = "dn.rda")
