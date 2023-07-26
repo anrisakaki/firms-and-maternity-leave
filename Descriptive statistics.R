886cec9653a9fb19e7b175300715946668e4e98c
Anri Sakakibara <anrisakaki@gmail.com>
2023-07-26 19:18:45 +0100

    Descriptive statistics

--- Descriptive statistics.R
+++ Descriptive statistics.R
@@ -1,8 +1,6 @@
 # Summary of avg. share of female workers for each yer 
 
-dn <- bind_rows(dn08, dn09, dn10, dn11, dn12, dn13, dn14, dn15, dn16, dn17, dn18) %>% 
-  group_by(tinh, huyen, xa, ma_thue) %>% 
-  mutate(id = cur_group_id())
+load("dn.rda")
 
 yearly_avg <- dn %>% 
   group_by(year) %>% 
@@ -11,7 +9,7 @@ yearly_avg <- dn %>%
             avg_finformal = mean(finformal, na.rm = T),
             avg_f_ss = mean(f_ss, na.rm = T))
 
-# Visualising data  
+# Summarising yearly trends  
 
 ggplot(yearly_avg, aes(x=factor(year), y=avg_fworkers*100)) + 
   geom_col() + 
@@ -34,3 +32,14 @@ ggplot(dplyr::filter(yearly_avg, year < 2016), aes(x=factor(year), y=avg_finform
         panel.background = element_blank()) + 
   labs(x = "Year",
        y = "Average share of informal workers, female (%)") 
+
+ggplot(dplyr::filter(yearly_avg, year < 2016), aes(x=factor(year), y=avg_f_ss*100)) + 
+  geom_col() + 
+  theme_bw() +
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) + 
+  labs(x = "Year",
+       y = "Average share of workers receiving SS, female (%)") 
