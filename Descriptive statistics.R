9d611bba4d7b5e3c5ca6af30c304294aeb7870da
Anri Sakakibara <anrisakaki@gmail.com>
2023-07-28 15:55:36 +0100

    Descriptive statistics

--- Descriptive statistics.R
+++ Descriptive statistics.R
@@ -43,3 +43,54 @@ ggplot(dplyr::filter(yearly_avg, year < 2016), aes(x=factor(year), y=avg_f_ss*10
         panel.background = element_blank()) + 
   labs(x = "Year",
        y = "Average share of workers receiving SS, female (%)") 
+
+# By type of firm 
+
+yearly_avg_type <- dn %>% 
+  group_by(year, type) %>% 
+  summarise(avg_fworkers_eoy = mean(fworkers_eoy, na.rm = T),
+            avg_finformal = mean(finformal, na.rm = T),
+            avg_f_ss = mean(f_ss, na.rm = T))  %>% 
+  filter(type != "NA")
+
+ggplot(dplyr::filter(yearly_avg_type, year < 2016), aes(x = year, y = (avg_f_ss)*100, colour = type)) +
+  geom_line(size = 1.2) +
+  geom_vline(xintercept = 2013, linetype = "dashed", color = "gray40") +
+  scale_x_continuous(breaks=seq(2008,2016,1)) +
+  labs(x = "Year",
+       y = "Share of workers w. SS, female (%)") +
+  theme_minimal() +
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank())
+ggsave(file = "N_fworkers_SS_type.png", device = png, width = 7, height = 7)
+
+ggplot(dplyr::filter(yearly_avg_type, year < 2016), aes(x = year, y = (avg_finformal)*100, colour = type)) +
+  geom_line(size = 1.2) +
+  geom_vline(xintercept = 2013, linetype = "dashed", color = "gray40") +
+  scale_x_continuous(breaks=seq(2008,2016,1)) +
+  labs(x = "Year",
+       y = "Share of informal workers, female (%)") +
+  theme_minimal() +
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank())
+ggsave(file = "N_finformal_SS_type.png", device = png, width = 7, height = 7)
+
+ggplot(yearly_avg_type, aes(x = year, y = (avg_fworkers_eoy)*100, colour = type)) +
+  geom_line(size = 1.2) +
+  geom_vline(xintercept = 2013, linetype = "dashed", color = "gray40") +
+  scale_x_continuous(breaks=seq(2008,2018,1)) +
+  labs(x = "Year",
+       y = "Share of workers, female (%)") +
+  theme_minimal() +
+  theme(axis.line = element_line(color='black'),
+        plot.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank())
+ggsave(file = "N_fworkers_type.png", device = png, width = 7, height = 7)
